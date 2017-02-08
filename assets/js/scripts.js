081d646267035c2fadfc3f74fd1a5b387a859169
Schizohatter <soanvig@gmail.com>
2017-02-08 16:10:45 +0100

    Init

---
+++ assets/js/scripts.js
@@ -0,0 +1,8 @@
+$('.scrollable').mCustomScrollbar({
+  scrollInertia: 0,
+  alwaysShowScrollbar: 2,
+  scrollButtons: { enable: true },
+  theme: "bg-theme",
+  autoDraggerLength: false,
+  scrollbarPosition: "outside"
+});
