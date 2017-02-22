e169ce7707ce62829eb0c0b1a55f00c9ba850046
Mortimer <soanvig@gmail.com>
2017-02-22 14:19:33 +0100

    Add simple tabs

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -6,3 +6,14 @@ $('.scrollable').mCustomScrollbar({
   autoDraggerLength: false,
   scrollbarPosition: "outside"
 });
+
+$('body').on('click', '.tabs__button', function () {
+	const $this = $(this);
+	const $tabsContainer = $this.closest('.tabs');
+	const target = $this.parent().data('target');
+	const $tabs = $tabsContainer.find('.tabs__tab');
+	$tabs.removeClass('tabs__tab--active');
+	$tabs.filter('[data-tab="' + target + '"]').addClass('tabs__tab--active')
+	$tabsContainer.find('.tabs__button').removeClass('tabs__button--active');
+	$this.addClass('tabs__button--active');
+});
